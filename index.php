197e81a22af5c2865f6bdfe2a994893c0e7a32de
KamilWojtalak <kamilwojtalak99@gmail.com>
2021-11-27 01:36:00 +0100

    Logging

--- index.php
+++ index.php
@@ -3,21 +3,13 @@
 use App\Exception\ExceptionHandler;
 use App\Helpers\App;
 use App\Helpers\Config;
+use App\Logger\Logger;
+use App\Logger\LogLevel;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 require_once __DIR__ . '/Src/Exception/exception.php';
 
-$db = new mysqli('dsfafasdsdaf', 'root', '', 'bug');
-exit;
+$logger = new Logger();
 
-$config = Config::getFileContent('asefsd');
-
-// $config = Config::get('app', 'log_path');
-$app = new App();
-
-echo $app->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
-echo $app->getLogPath() . PHP_EOL;
-echo $app->getEnvironment() . PHP_EOL;
-echo $app->isDebugMode() . PHP_EOL;
-echo $app->isRunningFromConsole() . PHP_EOL;
+$logger->log(LogLevel::WARNING, 'Warnigng message');
