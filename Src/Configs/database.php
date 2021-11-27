1b9e1e3bacfccf31ec7a9b292586c121e6c9b5c0
KamilWojtalak <kamilwojtalak99@gmail.com>
2021-11-27 18:21:41 +0100

    Test driven database connection 0 part 2 PDO

--- Src/Configs/database.php
+++ Src/Configs/database.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
-
+  'pdo' => [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'db_name' => 'udemy_bug_app',
+    'db_username' => 'root',
+    'db_user_password' => '',
+    'default_fetch' => PDO::FETCH_OBJ,
+  ], 
 ];
