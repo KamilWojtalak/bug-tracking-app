f20e33d7571599e4334940297316ef55d3410a0b
KamilWojtalak <kamilwojtalak99@gmail.com>
2021-12-03 02:10:19 +0100

    Creating an Entity

--- Tests/Units/RepositoryTest.php
+++ Tests/Units/RepositoryTest.php
@@ -1,4 +1,6 @@
-<?php declare( strict_types=1 );
+<?php
+
+declare(strict_types=1);
 
 
 namespace Tests\Units;
@@ -10,66 +12,68 @@ use PHPUnit\Framework\TestCase;
 
 class RepositoryTest extends TestCase
 {
-    /** @var QueryBuilder $queryBuilder */
-    private $queryBuilder;
-    private $bugReportRepository;
+  /** @var QueryBuilder $queryBuilder */
+  private $queryBuilder;
+  private $bugReportRepository;
 
-    public function setUp()
-    {
-        $this->queryBuilder = DbQueryBuilderFactory::make(
-            'database', 'pdo', ['db_name' => 'bug_app_testing']
-        );
-        $this->queryBuilder->beginTransaction();
+  public function setUp()
+  {
+    $this->queryBuilder = DbQueryBuilderFactory::make(
+      'database',
+      'pdo',
+      ['db_name' => 'bug_app_testing']
+    );
+    $this->queryBuilder->beginTransaction();
 
-        $bugReportRepository = new BugReportRepository($this->queryBuilder);
-        parent::setUp();
-    }
+    $bugReportRepository = new BugReportRepository($this->queryBuilder);
+    parent::setUp();
+  }
 
-    public function testItCanCreateRecordWithEntity()
-    {
-        $newBugReport = $this->createBugReport();
-        self::assertInstanceOf(BugReport::class, $newBugReport);
-        self::assertSame('Type 2', $newBugReport->getReportType());
-        self::assertSame('https://testing-link.com', $newBugReport->getLink());
-        self::assertSame('This is a dummy message', $newBugReport->getMessage());
-        self::assertSame('email@test.com', $newBugReport->getEmail());
-    }
+  public function testItCanCreateRecordWithEntity()
+  {
+    $newBugReport = $this->createBugReport();
+    self::assertInstanceOf(BugReport::class, $newBugReport);
+    self::assertSame('Type 2', $newBugReport->getReportType());
+    self::assertSame('https://testing-link.com', $newBugReport->getLink());
+    self::assertSame('This is a dummy message', $newBugReport->getMessage());
+    self::assertSame('email@test.com', $newBugReport->getEmail());
+  }
 
-    public function testItCanUpdateAGivenEntity()
-    {
-        $newBugReport = $this->createBugReport();
-        $bugReport = $this->bugReportRepository->find($newBugReport->getId());
-        $bugReport
-            ->setMessage('this is from update method')
-            ->setLink('https://newlink.com/image.png');
-        $updatedReport = $this->bugReportRepository->update($bugReport);
+  public function testItCanUpdateAGivenEntity()
+  {
+    $newBugReport = $this->createBugReport();
+    $bugReport = $this->bugReportRepository->find($newBugReport->getId());
+    $bugReport
+      ->setMessage('this is from update method')
+      ->setLink('https://newlink.com/image.png');
+    $updatedReport = $this->bugReportRepository->update($bugReport);
 
-        self::assertInstanceOf(BugReport::class, $newBugReport);
-        self::assertSame('this is from update method', $newBugReport->getLink());
-        self::assertSame('https://newlink.com/image.png', $newBugReport->getMessage());
-    }
+    self::assertInstanceOf(BugReport::class, $newBugReport);
+    self::assertSame('this is from update method', $newBugReport->getLink());
+    self::assertSame('https://newlink.com/image.png', $newBugReport->getMessage());
+  }
 
-    public function testItCanDeleteAGivenEntity()
-    {
-        $newBugReport = $this->createBugReport();
-        $this->bugReportRepository->delete($newBugReport);
-        $bugReport = $this->bugReportRepository->find($newBugReport->getId());
-        self::assertNull($bugReport);
-    }
+  public function testItCanDeleteAGivenEntity()
+  {
+    $newBugReport = $this->createBugReport();
+    $this->bugReportRepository->delete($newBugReport);
+    $bugReport = $this->bugReportRepository->find($newBugReport->getId());
+    self::assertNull($bugReport);
+  }
 
-    private function createBugReport(): BugReport
-    {
-        $bugReport = new BugReport();
-        $bugReport->setReportType('Type 2')
-            ->setLink('https://testing-link.com')
-            ->setMessage('This is a dummy message')
-            ->setEmail('email@test.com');
-        return $this->bugReportRepository->create($bugReport);
-    }
+  private function createBugReport(): BugReport
+  {
+    $bugReport = new BugReport();
+    $bugReport->setReportType('Type 2')
+      ->setLink('https://testing-link.com')
+      ->setMessage('This is a dummy message')
+      ->setEmail('email@test.com');
+    return $this->bugReportRepository->create($bugReport);
+  }
 
-    public function tearDown()
-    {
-        $this->queryBuilder->rollback();
-        parent::tearDown();
-    }
+  public function tearDown()
+  {
+    $this->queryBuilder->rollback();
+    parent::tearDown();
+  }
 }
